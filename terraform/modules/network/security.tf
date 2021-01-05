4825f0619ccc33d5c40285e742ef5ca87b95cf77
yusuke.yoda <yusuke.yoda@givery.co.jp>
2021-01-05 12:38:03 +0900

    Allog tcp port range fo r server

--- terraform/modules/network/security.tf
+++ terraform/modules/network/security.tf
@@ -141,6 +141,13 @@ resource aws_security_group eks_worker_group_mgmt_one {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 7000
+    to_port     = 8000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
